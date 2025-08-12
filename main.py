ab9fa6abe200c8f1dfb9842700b7bb1f42480ddc
olenevoid <mazekunov@gmail.com>
2025-08-12 16:19:52 +0500

    Add fetch_xkcd_ post function

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from environs import env
+from image_helpers import IMAGE_FOLDER_NAME, save_image, get_filename_from_url
+from os import makedirs, path
+import requests
+from typing import TypeAlias
+
+
+ImageUrl: TypeAlias = str
+Commentary: TypeAlias = str
+
+
+def fetch_xkcd_post(post_id: int) -> tuple[ImageUrl, Commentary]:
+    image_url = f'https://xkcd.com/{post_id}/info.0.json'
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    xkcd_post = response.json()
+
+    return (xkcd_post.get('img'), xkcd_post.get('alt'))
+
+
 
 
 def main():

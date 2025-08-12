f607ee0f21135187b82ea3e828cf7e46298c9382
olenevoid <mazekunov@gmail.com>
2025-08-12 16:20:14 +0500

    Add download_xkcd_image function

--- main.py
+++ main.py
@@ -20,11 +20,18 @@ def fetch_xkcd_post(post_id: int) -> tuple[ImageUrl, Commentary]:
     return (xkcd_post.get('img'), xkcd_post.get('alt'))
 
 
+def download_xkcd_image(url: str):
+    filepath = path.join(IMAGE_FOLDER_NAME, get_filename_from_url(url))
+    save_image(url, filepath)
 
 
 def main():
     env.read_env()
     tg_token = env('TG_TOKEN')
+    makedirs(IMAGE_FOLDER_NAME, exist_ok=True)
+    url, comment = fetch_xkcd_post(666)
+    print('comment', comment)
+    download_xkcd_image(url)
 
 
 if __name__ == '__main__':
